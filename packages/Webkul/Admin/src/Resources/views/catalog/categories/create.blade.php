5e2223435c13b751c3a17a3a477653e02f8ff789
Phyo Thiha <phyothiha96@gmail.com>
2024-01-05 01:08:26 +0630

    make customer can edit slug

--- packages/Webkul/Admin/src/Resources/views/catalog/categories/create.blade.php
+++ packages/Webkul/Admin/src/Resources/views/catalog/categories/create.blade.php
@@ -81,7 +81,6 @@
                                 :class="[errors['{{ 'name' }}'] ? 'border border-red-600 hover:border-red-600' : '']"
                                 class="flex w-full min-h-[39px] py-2 px-3 border rounded-md text-sm text-gray-600 dark:text-gray-300 transition-all hover:border-gray-400 dark:hover:border-gray-400 focus:border-gray-400 dark:focus:border-gray-400 dark:bg-gray-900 dark:border-gray-800"
                                 placeholder="{{ trans('admin::app.catalog.categories.create.name') }}"
-                                v-slugify-target:slug="setValues"
                             >
                         </v-field>
 
@@ -238,8 +237,6 @@
                                     :class="[errors['{{ 'slug' }}'] ? 'border border-red-600 hover:border-red-600' : '']"
                                     class="flex w-full min-h-[39px] py-2 px-3 border rounded-md text-sm text-gray-600 dark:text-gray-300 transition-all hover:border-gray-400 dark:hover:border-gray-400 focus:border-gray-400 dark:focus:border-gray-400 dark:bg-gray-900 dark:border-gray-800"
                                     placeholder="{{ trans('admin::app.catalog.categories.create.slug') }}"
-                                    v-slugify-target:slug
-                                    readonly
                                 >
                             </v-field>
 
--- packages/Webkul/Admin/src/Resources/views/catalog/categories/edit.blade.php
+++ packages/Webkul/Admin/src/Resources/views/catalog/categories/edit.blade.php
@@ -115,7 +115,6 @@
                                 :class="[errors['{{ $currentLocale->code }}[name]'] ? 'border border-red-600 hover:border-red-600' : '']"
                                 class="flex w-full min-h-[39px] py-2 px-3 border rounded-md text-sm text-gray-600 dark:text-gray-300 transition-all hover:border-gray-400 dark:hover:border-gray-400 focus:border-gray-400 dark:focus:border-gray-400 dark:bg-gray-900 dark:border-gray-800"
                                 placeholder="{{ trans('admin::app.catalog.categories.edit.name') }}"
-                                v-slugify-target:{{$currentLocale->code . '[slug]'}}="setValues"
                             >
                         </v-field>
 
@@ -279,8 +278,6 @@
                                     :class="[errors['{{$currentLocale->code}}[slug]'] ? 'border border-red-600 hover:border-red-600' : '']"
                                     class="flex w-full min-h-[39px] py-2 px-3 border rounded-md text-sm text-gray-600 dark:text-gray-300 transition-all hover:border-gray-400 dark:hover:border-gray-400 focus:border-gray-400 dark:focus:border-gray-400 dark:bg-gray-900 dark:border-gray-800"
                                     placeholder="{{ trans('admin::app.catalog.categories.edit.slug') }}"
-                                    v-slugify-target:slug
-                                    readonly
                                 >
                             </v-field>
 
